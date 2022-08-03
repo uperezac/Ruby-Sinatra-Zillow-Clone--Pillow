ae13b971a8bea58d8012e84ce75d9da79193e4da
Ulises Perez Acosta <u.perezacosta@gmail.com>
2022-08-03 18:43:19 -0400

    added methods to post new listing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,44 @@ class ApplicationController < Sinatra::Base
       "Hello World!"
   end
 
-  get '/listings' do 
+  get '/listing' do 
       listings_all = Listing.all
       listings_all.to_json
   end
 
-  get '/cities' do
+  get '/city' do
     cities_all = City.all
     cities_all.to_json
   end
 
-  get '/agents' do
+  get '/agent' do
     agents_all = Agent.all
     agents_all.to_json
+  end
+
+  post '/listing/newlisting' do
+    picture = params[:picture_url]
+    address = params[:addy]
+    city = params[:city.name]
+    zipcode = params[:zipcode]
+    buy = params[:to_buy]
+    rent = params[:to_rent]
+    price = params[:price]
+    sqrft = params[:sqrft]
+    bedrooms = params[:bed]
+    bathrooms = params[:bath]
+    built = params[:built]
+    laundry = params[:laundry]
+    superintendent = params[:super]
+    agent = params[:agent]
+
+    listing = Listing.create(addy:address, picture_url:picture, bed:bedrooms, bath:bathrooms, zipcode:zipcode,built:built, to_buy:buy to_rent:buy,laundry:,price: 2506,sqrft: 729,super:superintendent,city_id:city,agent_id:agent)
+
+    {
+      success:true,
+      data: listing
+    }.to_json
+
   end
 
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,5 @@
 class City < ActiveRecord::Base
     has_many :listings
     has_many :agents, through: :listings
-  
+
 end
